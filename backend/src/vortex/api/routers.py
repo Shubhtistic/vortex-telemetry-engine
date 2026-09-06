ab946133d85999503fd4d2bebefda5eb69807a0a
Shubhtistic <theshubham1412@gmail.com>
2026-09-06 22:35:08 +0530

    Add: add users endpoints into api module

--- backend/src/vortex/api/routers.py
+++ backend/src/vortex/api/routers.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from src.vortex.organizations.routers import router as org_router
 from src.vortex.auth.routers import router as auth_router
 from src.vortex.tenants.routers import router as tenants_router
+from src.vortex.users.routers import router as user_router
 
 router = APIRouter()
 
 router.include_router(org_router)
 router.include_router(auth_router)
 router.include_router(tenants_router)
+router.include_router(user_router)
